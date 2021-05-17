15563aad0c96479efcad0f095ba34835f4a5c22b
Obed Chaca <obed.chaca@tecsup.edu.pe>
2021-05-17 15:36:39 -0500

    first commit

---
+++ Lab11MVVM/Lab11MVVM/Lab11MVVM/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Lab11MVVM.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Lab11MVVM
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Item1.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new Operations());
+            };
+        }
+    }
+}
